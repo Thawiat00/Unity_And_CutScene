2d0a327210f4f9e8af1116e6a4e3df61ec8e8107
Thawiat00 <Thawiat01@gmail.com>
2024-10-19 19:29:31 +0700

    code update dynamic choice button
    Use dynamically loaded data on all three choice buttons (clear)
    
    Answer with the second sentence of the choice button (clear) question.

--- Assets/CODE/CutScene_Choice/dynamic/BreakChoiceData_dynamic.cs
+++ Assets/CODE/CutScene_Choice/dynamic/BreakChoiceData_dynamic.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 [System.Serializable]
 public class BreakChoiceData_dynamic
 {
-    public List<BreakChoiceScene_dynamic> break_choice_scene; // List ??? break_choice_scene
-    public List<DialogData_dynamic> dialog_cs_OC; // List ??? dialog data
+    public List<BreakChoiceScene_dynamic> break_choice_scene; // List ?????? break_choice_scene
+    public List<DialogData_dynamic> dialog_cs_OC; // List ?????? dialog data
 }
 
 [System.Serializable]
 public class BreakChoiceScene_dynamic
 {
-    public string key; // ??????????? scene (???? break_choice_scene_1)
-    public List<SceneData_dynamic> sceneData; // List ??????????? scene
+    public string key; // ???? scene (???? break_choice_scene_1)
+    public List<SceneData_dynamic> sceneData; // List ?????? scene data
 }
 
 [System.Serializable]
 public class SceneData_dynamic
 {
-    public List<OptionCallAll_dynamic> option_call_all; // ???????????????
-    public List<OptionCallAllAnswer_dynamic> option_call_all_answer; // ????????????
+    public List<OptionCallAll_dynamic> option_call_all;
+    public List<OptionCallAllAnswer_dynamic> option_call_all_answer;
 }
 
 [System.Serializable]
 public class OptionCallAll_dynamic
 {
-    public List<string> option_call_option_1; // ???????? 1
-    public List<string> option_call_option_2; // ???????? 2
-    public List<string> option_call_option_3; // ???????? 3
+    public List<string> option_call_option_1;
+    public List<string> option_call_option_2;
+    public List<string> option_call_option_3;
 }
 
 [System.Serializable]
 public class OptionCallAllAnswer_dynamic
 {
-    public List<string> option_answer_option_1; // ????? 1
-    public List<string> option_answer_option_2; // ????? 2
-    public List<string> option_answer_option_3; // ????? 3
+    public List<string> option_answer_option_1;
+    public List<string> option_answer_option_2;
+    public List<string> option_answer_option_3;
 }
 
 [System.Serializable]
 public class DialogData_dynamic
 {
-    public List<string> cutscene_choice; // ???????????? cutscene choices
-    public string End_dialog; // ?????????????????
+    public List<string> cutscene_choice; // ?????? choices ??? scene
+    public string End_dialog;
 }
