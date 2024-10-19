ea303ca25730639401c705d42cfeaf4588c94182
Thawiat00 <Thawiat01@gmail.com>
2024-10-19 20:32:42 +0700

    code update  dialog button choice  (after that, end the loop (clear))
    In the dialogtext, play the sentence. option_call_option of the second sentence and the sentences after that, end the loop (clear)

--- Assets/CODE/CutScene_Choice/dynamic/BreakChoiceLoader_dynamic.cs
+++ Assets/CODE/CutScene_Choice/dynamic/BreakChoiceLoader_dynamic.cs
@@ -1,30 +1,32 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // ????????? TextMeshPro ??? TMPro; ???
+using UnityEngine.UI; // TextMeshPro ?? TMPro; ???
 
 public class BreakChoiceLoader_dynamic : MonoBehaviour
 {
-    public BreakChoiceData_dynamic breakChoiceData; // ?????? JSON ???????
-    public Button choiceButton1; // ??????? 1
-    public Button choiceButton2; // ??????? 2
-    public Button choiceButton3; // ??????? 3
-    public Text dialogText; // UI Text ???????????????? (??? TextMeshPro ???)
+    public BreakChoiceData_dynamic breakChoiceData; // JSON Data
+    public Button choiceButton1; // Choice Button 1
+    public Button choiceButton2; // Choice Button 2
+    public Button choiceButton3; // Choice Button 3
+    public Text dialogText; // UI Text for dialog (or TextMeshPro)
+
+    private List<string> currentDialogOptions; // To store the current dialog options
+    private int currentDialogIndex = 0; // To keep track of the current dialog index
 
     void Start()
     {
         LoadBreakChoiceScene();
-        SetupButtons(); // ????????????????????????
+        SetupButtons(); // Set up the buttons
     }
 
     void LoadBreakChoiceScene()
     {
-        TextAsset json = Resources.Load<TextAsset>("mock_break_choice_scene"); // ???????? JSON
+        TextAsset json = Resources.Load<TextAsset>("mock_break_choice_scene"); // Load JSON
         if (json != null)
         {
-            breakChoiceData = JsonUtility.FromJson<BreakChoiceData_dynamic>(json.text); // ?????????? JSON
+            breakChoiceData = JsonUtility.FromJson<BreakChoiceData_dynamic>(json.text); // Deserialize JSON
             if (breakChoiceData.break_choice_scene.Count > 0)
             {
-                // ????????????????????????????
                 Debug.Log("Break choice scenes loaded successfully.");
             }
             else
@@ -42,22 +44,52 @@ public class BreakChoiceLoader_dynamic : MonoBehaviour
     {
         if (breakChoiceData.break_choice_scene.Count > 0)
         {
-            var firstScene = breakChoiceData.break_choice_scene[0].sceneData[0]; // ??????? scene ???
+            // Loop through all scenes (this can be customized)
+            foreach (var scene in breakChoiceData.break_choice_scene)
+            {
+                var firstScene = scene.sceneData[0]; // Access the first scene of each BreakChoiceScene
 
-            // ??????????????????
-            choiceButton1.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_1[0]; // ????????????????? 1
-            choiceButton2.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_2[0]; // ????????????????? 2
-            choiceButton3.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_3[0]; // ????????????????? 3
+                // Set button text for all choices
+                choiceButton1.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_1[0];
+                choiceButton2.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_2[0];
+                choiceButton3.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_3[0];
 
-            // ????? listener ???????
-            choiceButton1.onClick.AddListener(() => UpdateDialogText(firstScene.option_call_all[0].option_call_option_1[1]));
-            choiceButton2.onClick.AddListener(() => UpdateDialogText(firstScene.option_call_all[0].option_call_option_2[1]));
-            choiceButton3.onClick.AddListener(() => UpdateDialogText(firstScene.option_call_all[0].option_call_option_3[1]));
+                // Add listeners for each button
+                choiceButton1.onClick.AddListener(() => SelectOption(firstScene.option_call_all[0].option_call_option_1));
+                choiceButton2.onClick.AddListener(() => SelectOption(firstScene.option_call_all[0].option_call_option_2));
+                choiceButton3.onClick.AddListener(() => SelectOption(firstScene.option_call_all[0].option_call_option_3));
+            }
         }
+    }
+
+
+    void SelectOption(List<string> selectedOption)
+    {
+        currentDialogOptions = selectedOption; // Store selected option
+        currentDialogIndex = 1; // Start from the second line of the selected option
+        UpdateDialogText(currentDialogOptions[currentDialogIndex]); // Display the second line
+
+        // Hide the choice buttons
+        choiceButton1.gameObject.SetActive(false);
+        choiceButton2.gameObject.SetActive(false);
+        choiceButton3.gameObject.SetActive(false);
     }
 
     void UpdateDialogText(string text)
     {
-        dialogText.text = text; // ??????????????? dialogText
+        dialogText.text = text; // Update dialogText
+    }
+
+    void Update()
+    {
+        // Check for spacebar input
+        if (Input.GetKeyDown(KeyCode.Space) && currentDialogOptions != null)
+        {
+            if (currentDialogIndex < currentDialogOptions.Count - 1) // Check if there are more lines
+            {
+                currentDialogIndex++; // Move to the next line
+                UpdateDialogText(currentDialogOptions[currentDialogIndex]); // Display the next line
+            }
+        }
     }
 }
