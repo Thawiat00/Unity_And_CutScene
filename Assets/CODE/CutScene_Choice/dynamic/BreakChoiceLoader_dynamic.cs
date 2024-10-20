2ffe8fce0bf164f254747ffde004a5fc92c3eb58
Thawiat00 <Thawiat01@gmail.com>
2024-10-21 01:53:13 +0700

    update code dialog cutscene button
    -can change sequent order on json dialog_cs_OC and can update first text order to display fist conversation of option_call_option_1,2,3 to display button 1,2,3

--- Assets/CODE/CutScene_Choice/dynamic/BreakChoiceLoader_dynamic.cs
+++ Assets/CODE/CutScene_Choice/dynamic/BreakChoiceLoader_dynamic.cs
@@ -1,10 +1,17 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI; // TextMeshPro ?? TMPro; ???
+using static DialogManager_choice;
+using static UnityEngine.GraphicsBuffer;
 
 public class BreakChoiceLoader_dynamic : MonoBehaviour
 {
     public BreakChoiceData_dynamic breakChoiceData; // JSON Data
+  
     public Button choiceButton1; // Choice Button 1
     public Button choiceButton2; // Choice Button 2
     public Button choiceButton3; // Choice Button 3
@@ -15,19 +22,20 @@ public class BreakChoiceLoader_dynamic : MonoBehaviour
 
     void Start()
     {
-        LoadBreakChoiceScene();
+        // LoadBreakChoiceScene();
         SetupButtons(); // Set up the buttons
     }
 
     void LoadBreakChoiceScene()
     {
-        TextAsset json = Resources.Load<TextAsset>("mock_break_choice_scene"); // Load JSON
+        TextAsset json = Resources.Load<TextAsset>("mock_break_choice_scene"); // ???? JSON
         if (json != null)
         {
-            breakChoiceData = JsonUtility.FromJson<BreakChoiceData_dynamic>(json.text); // Deserialize JSON
+            breakChoiceData = JsonUtility.FromJson<BreakChoiceData_dynamic>(json.text); // ???? JSON ??????????
             if (breakChoiceData.break_choice_scene.Count > 0)
             {
                 Debug.Log("Break choice scenes loaded successfully.");
+                LogDialogTopics(); // ??????????????????????????????? dialog_cs_OC
             }
             else
             {
@@ -40,27 +48,191 @@ public class BreakChoiceLoader_dynamic : MonoBehaviour
         }
     }
 
+    void LogDialogTopics()
+    {
+       
+
+        foreach (var scene in breakChoiceData.dialog_cs_OC)
+        {
+            foreach (var dialog in scene.cutscene_choice)
+            {
+                Debug.Log("Dialog Topic: " + dialog); // ?????????? dialog
+            }
+        }
+    }
+
+
     void SetupButtons()
     {
-        if (breakChoiceData.break_choice_scene.Count > 0)
+        LoadDataJson();
+
+        CheckDataForUseCurrent(); // ?????????????????????????????
+
+        UseFirstDataCurrentForFirstLoadButton();
+
+    }
+
+    void UseFirstDataCurrentForFirstLoadButton()
+    {
+
+        // ???????????????????? breakChoiceData.dialog_cs_OC ???????
+        if (breakChoiceData.dialog_cs_OC[0].cutscene_choice.Count > 0)
         {
-            // Loop through all scenes (this can be customized)
-            foreach (var scene in breakChoiceData.break_choice_scene)
+            // var firstDialog = breakChoiceData.dialog_cs_OC[0]; // ????????????? dialog ??????
+            var firstDialog = breakChoiceData.dialog_cs_OC[0].cutscene_choice[0]; // ????????????? dialog ??????
+
+            Debug.Log("firstDialog :" + firstDialog);
+          //  Debug.Log("firstDialog :" + firstDialog);
+
+            // ?????????????????? break_choice_scene ??? firstDialog
+            LinkDataSameKey(firstDialog);
+        }
+    }
+
+    void LinkDataSameKey(string firstDialog)
+    {
+
+
+        // ??????? break_choice_scene
+
+        //foreach breakChoiceData.break_choice_scene
+        foreach (var scene in breakChoiceData.break_choice_scene)
+        {
+            
+          
+                if (scene.key.Contains(firstDialog) == true)
+                {
+                   
+
+                    Debug.Log("scene.key.Contains(dialogChoice) have :" + firstDialog);
+
+                    int index = breakChoiceData.break_choice_scene.FindIndex(scene => scene.key.Contains(firstDialog));
+
+                Debug.Log("breakChoiceData.break_choice_scene.FindIndex(scene => scene.key.Contains(firstDialog)); :" + index);
+
+
+                // ????????????? sceneData ????????? index
+                var matchedScene = breakChoiceData.break_choice_scene[index];
+
+                Debug.Log("breakChoiceData.break_choice_scene[index] :" + matchedScene.key.ToString());
+
+             
+                // scene.key[index] = scene.sceneData[index].option_call_all
+                string buttonText1 = matchedScene.sceneData[0].option_call_all[0].option_call_option_1[0];
+                string buttonText2 = matchedScene.sceneData[0].option_call_all[0].option_call_option_2[0];
+                string buttonText3 = matchedScene.sceneData[0].option_call_all[0].option_call_option_3[0];
+
+                Debug.Log("buttonText1 :" + buttonText1);
+                Debug.Log("buttonText2 :" + buttonText2);
+                Debug.Log("buttonText3 :" + buttonText3);
+
+
+                // ??????????????????????????????????????
+                choiceButton1.GetComponentInChildren<Text>().text = buttonText1;
+                choiceButton2.GetComponentInChildren<Text>().text = buttonText2;
+                choiceButton3.GetComponentInChildren<Text>().text = buttonText3;
+
+                // ??? break ?????????
+                break;
+            }    
+               
+
+        }
+
+
+    }
+
+
+    void LinkDataSameKey2(string firstDialog)
+    {
+        find_sceneData_with_string();
+
+        /*
+        // Loop ???? break_choice_scene ??????????????????
+        foreach (var scene in breakChoiceData.break_choice_scene)
+        {
+            var firstScene = scene.sceneData[0]; // ??????? sceneData ???
+
+            // ??????????????????????????????? option_call_option_ ?? firstScene
+            if (firstScene.option_call_all.Count > 0)
             {
-                var firstScene = scene.sceneData[0]; // Access the first scene of each BreakChoiceScene
-
-                // Set button text for all choices
-                choiceButton1.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_1[0];
-                choiceButton2.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_2[0];
-                choiceButton3.GetComponentInChildren<Text>().text = firstScene.option_call_all[0].option_call_option_3[0];
-
-                // Add listeners for each button
                 choiceButton1.onClick.AddListener(() => SelectOption(firstScene.option_call_all[0].option_call_option_1));
                 choiceButton2.onClick.AddListener(() => SelectOption(firstScene.option_call_all[0].option_call_option_2));
                 choiceButton3.onClick.AddListener(() => SelectOption(firstScene.option_call_all[0].option_call_option_3));
             }
         }
+        */
     }
+
+    private void find_sceneData_with_string()
+    {
+        throw new NotImplementedException();
+    }
+
+   
+
+
+    void LoadDataJson()
+    {
+     // ???????? JSON ??? Resources
+        TextAsset json = Resources.Load<TextAsset>("mock_break_choice_scene"); // ????????????????????????????
+        if (json != null)
+        {
+            // ???? JSON ??????????????????????
+            breakChoiceData = JsonUtility.FromJson<BreakChoiceData_dynamic>(json.text);
+
+          
+
+            if (breakChoiceData != null && breakChoiceData.break_choice_scene.Count > 0)
+            {
+                Debug.Log("load JSON successandhavedata break_choice_scene.");
+
+                // ???????????????????????????????????????????????????
+                // ???? ??????? label ???????, ????? event listener ???????
+            }
+         
+            else
+            {
+               Debug.LogError("notfound data break_choice_scene in JSON.");
+            }
+
+        }
+
+        else
+        {
+            Debug.LogError("cannot dowload JSON .");
+
+        }
+
+
+
+    }
+
+
+    private void CheckDataForUseCurrent()
+    {
+        // ??????????????????????????????????????
+        if (breakChoiceData != null && breakChoiceData.dialog_cs_OC[0].cutscene_choice.Count > 0)
+        {
+            foreach (string choice in breakChoiceData.dialog_cs_OC[0].cutscene_choice)
+            {
+                Debug.Log("data dialog_cs_OC .cutscene_choice ready : " + choice); // ??????????? choice
+            }
+
+           // Debug.Log("data dialog_cs_OC .cutscene_choice ready :" + breakChoiceData.dialog_cs_OC[0].cutscene_choice.na);
+            // ???????????????????????? ????????????????????????????????????????
+            // ???? ????????????? dialog ?????????? scene
+        }
+
+        else
+        {
+            Debug.LogError("data dialog_cs_OC .cutscene_choice cannot use or have error in dowload");
+        }
+
+
+    }
+
+
 
 
     void SelectOption(List<string> selectedOption)
